9b40d6a51510e77560a9ddc2fd47bb49d095af9a
rngprw <163730638+rngprw@users.noreply.github.com>
2024-03-19 14:38:00 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+my_age =int(input("Please tell me your age : "))
+
+for future in [10, 20, 30] :
+    future_age = my_age + future 
+    print(f" In {future} years, you will be {future_age} years old.")
+
