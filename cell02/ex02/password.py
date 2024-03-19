00e5d3f11fd620451fc35efe9e52691b4c96bce7
rngprw <163730638+rngprw@users.noreply.github.com>
2024-03-19 10:09:57 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+entered_password =input("password to be entered? : ")
+if entered_password==password :
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
