23e0c4a7cdb90521d62357234eb418dd0bd57e1b
rngprw <163730638+rngprw@users.noreply.github.com>
2024-03-19 09:36:43 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,11 @@
+num = int(input("number to be entered? :" ))
+if num > 0 :
+        print("this number is positive")
+elif num < 0 :
+       print("this number is negative")
+elif num == 0 :
+        print("this number is both positie and negative")
+
+        
+
+
