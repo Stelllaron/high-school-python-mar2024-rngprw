4382be8e32d4387fe5a35f59430cabecffb51c0d
rngprw <163730638+rngprw@users.noreply.github.com>
2024-03-19 15:02:03 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Give me the first number : "))
+num2 = int(input("Give me the second number : "))
+
+add = num1 + num2
+sub = num1 - num2
+product = num1 * num2
+divide =num1/num2
+
+print("thank you!")
+print(f"{num1} + {num2} = {add}")
+print(f"{num1} - {num2} = {sub}")
+print(f"{num1} x {num2} = {product}")
+print(f"{num1} / {num2} = {divide}") 
