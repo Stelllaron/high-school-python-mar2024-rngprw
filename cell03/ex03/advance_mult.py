67f5efaa2abf7445ad40e6d1767d5709a446d53d
rngprw <163730638+rngprw@users.noreply.github.com>
2024-03-19 12:55:35 +0700

    Add files via upload

---
+++ cell03/ex03/advance_mult.py
@@ -0,0 +1,13 @@
+table = 0
+
+while table <= 10:
+    number = 0
+    
+    table_line = f"Table de {table}:"
+    
+    while number <= 10:
+        table_line += f" {table * number}"
+        number += 1
+
+    print(table_line)
+    table += 1
