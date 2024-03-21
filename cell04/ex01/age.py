43b8af0ffcb69723fc10685e5bd268ee97f33bee
rngprw <163730638+rngprw@users.noreply.github.com>
2024-03-21 10:36:17 +0700

    age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 my_age =int(input("Please tell me your age : "))
-
+print(f"You are currently {my_age} years old")
 for future in [10, 20, 30] :
     future_age = my_age + future 
     print(f" In {future} years, you will be {future_age} years old.")
